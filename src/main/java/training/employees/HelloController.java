c7eb249bca7b7d3a9f79a4e1f7a666f288558587
sanyacs <sanyacs@freemail.hu>
2020-03-02 16:52:38 +0100

    First commit

---
+++ src/main/java/training/employees/HelloController.java
@@ -0,0 +1,22 @@
+package training.employees;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    private final HelloService helloService;
+
+    public HelloController(HelloService helloService) {
+        this.helloService = helloService;
+    }
+
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String sayHello() {
+        return helloService.sayHello().toUpperCase();
+    }
+
+}
---
+++ src/test/java/training/employees/HelloControllerIT.java
@@ -0,0 +1,18 @@
+package training.employees;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+@SpringBootTest
+public class HelloControllerIT {
+
+    @Autowired
+    HelloController helloController;
+
+    @Test
+    void testSayhello() {
+        assertThat(helloController.sayHello()).startsWith("HELLO SPRING BOOT");
+    }
+}
